d5e4b703e332d0971d054620df8374fbe0528f9b
Boris Tveritnev <boris.tveritnev@xing.com>
2016-09-30 17:43:15 +0200

    isbn as book id - green bar

--- src/main/java/catalog/models/Book.java
+++ src/main/java/catalog/models/Book.java
@@ -1,7 +1,27 @@
 package catalog.models;
 
 public class Book {
-	public boolean isValid(){
+	private String id;
+	private String isbn;
+
+	public Book() {
+		this("");
+	}
+
+	public Book(String isbn) {
+		this.isbn = isbn;
+		this.id = isbn;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public boolean isValid() {
 		return false;
 	}
 }
